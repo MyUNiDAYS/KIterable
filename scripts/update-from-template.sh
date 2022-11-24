82fecc2c8df9f377fdd270b3c0304c10ffaa1d39
Andrew Reed <andrew_reed@hotmail.com>
2022-11-24 17:27:37 +0000

    Adjusted script

--- scripts/update-from-template.sh
+++ scripts/update-from-template.sh
@@ -21,10 +21,10 @@ git checkout --orphan our_template $COMMIT_LATEST_TAG
 git add *
 git commit -m "Updated with the template version $LATEST_TAG_FLASK_APP_TEMPLATE"
 
-# Force a merge w/ the 'dev' branch
-git checkout dev
-git merge our_template --allow-unrelated-histories -X theirs
+# Force a merge w/ the 'master' branch
+git checkout master
+git merge our_template --allow-unrelated-histories -X ours
 
-# Remove orphan branch & push 'dev' branch
+# Remove orphan branch & push 'master' branch
 git branch -D our_template
-git push origin dev
+git push origin master
