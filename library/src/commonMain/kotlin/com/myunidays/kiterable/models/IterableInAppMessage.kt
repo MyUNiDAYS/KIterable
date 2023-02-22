46a7506310d75c4307a71cfff23982da9977148d
Jacob Hughes <jacoblbhughes@gmail.com>
2023-02-22 17:42:34 +0000

    > Expose Firebase RemoteMessage by changing iterable dependency to api (#5)
    > Add createdAt and expiresAt to IterableInAppMessage data class
    > Version bump to 0.0.18

--- library/src/commonMain/kotlin/com/myunidays/kiterable/models/IterableInAppMessage.kt
+++ library/src/commonMain/kotlin/com/myunidays/kiterable/models/IterableInAppMessage.kt
@@ -3,4 +3,6 @@ package com.myunidays.kiterable.models
 expect class IterableInAppMessage {
     val messageId: String
     val customPayload: Payload?
+    val createdAt: String?
+    val expiresAt: String?
 }
