309adac402323eec0b3d28573ba274453ea86237
Jacob Hughes <jacoblbhughes@gmail.com>
2023-05-05 12:27:24 +0100

    Enhancement remove unused interface method (#8)
    * Remove unused interface method on IosIterableApiInterface
    
    * Version 0.0.21
    
    * Remove unused import

--- library/src/iosMain/kotlin/com/myunidays/kiterable/IosIterableApiInterface.kt
+++ library/src/iosMain/kotlin/com/myunidays/kiterable/IosIterableApiInterface.kt
@@ -2,7 +2,6 @@ package com.myunidays.kiterable
 
 import com.myunidays.kiterable.models.IterableActionHandler
 import com.myunidays.kiterable.models.IterableConfig
-import com.myunidays.kiterable.models.IterableInAppMessage
 import com.myunidays.kiterable.models.PayloadData
 import platform.Foundation.NSData
 import platform.UIKit.UIApplicationLaunchOptionsKey
@@ -14,7 +13,6 @@ interface IosIterableApiInterface {
     fun setEmail(email: String?)
     fun getPayloadData(): PayloadData?
     fun getPayloadData(key: String): String?
-    fun getMessages(): List<IterableInAppMessage>
     fun getAndTrackDeepLink(uri: String, onCallback: IterableActionHandler)
     fun register(token: NSData)
     fun disableDeviceForCurrentUser()
